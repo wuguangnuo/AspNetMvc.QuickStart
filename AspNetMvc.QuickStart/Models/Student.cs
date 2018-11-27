047f6ef286077c7a12b746a331f43013981e533f
吴广诺 <wuguangnuo@qq.com>
2018-11-27 16:30:11 +0800

    更改模型并更新到数据库

--- AspNetMvc.QuickStart/Models/Student.cs
+++ AspNetMvc.QuickStart/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,20 @@ using System.Web;
 namespace AspNetMvc.QuickStart.Models
 {
     public class Student
+
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; }
+
         public int Gender { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string Major { get; set; }
+
         public DateTime EntranceDate { get; set; }
     }
     public class StudentDbContext : DbContext
